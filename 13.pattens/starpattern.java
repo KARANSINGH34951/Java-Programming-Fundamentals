7ea1f17a18b71458f3b56f427bd180daac7dc6f8
karan singh D <ceitkaransingh2026@gmail.com>
2024-08-11 20:35:41 +0530

    Added advanve pattern

--- 13.pattens/starpattern.java
+++ 13.pattens/starpattern.java
@@ -1,13 +1,37 @@
 public class starpattern {
   public static void main(String[] args) {
     int n=5;
+    char ch='A';
 
     for(int i=1;i<=n;i++){
       for(int j=1;j<=i;j++){
-        System.out.print("*");
+        System.out.print(ch);
+        ch++;
       }
       System.out.println();
     }
+
+
+    // for(int i=1;i<=n;i++){
+    //   for(int j=1;j<i;j++){
+    //     System.out.print(j);
+    //   }
+    //   System.out.println();
+    // }
+
+    // for(int i=0;i<n;i++){
+    //   for(int j=i;j<n;j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+
+    // for(int i=1;i<=n;i++){
+    //   for(int j=1;j<=i;j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
 
   }
 }
