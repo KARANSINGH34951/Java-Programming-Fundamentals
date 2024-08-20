d3099b2ccf039504dda176b32c8625a895dc6a74
karan singh D <ceitkaransingh2026@gmail.com>
2024-08-20 23:10:40 +0530

    Added array based questions

--- 16.Arrays/pairs.java
+++ 16.Arrays/pairs.java
@@ -1,5 +1,23 @@
 public class pairs {
   public static void main(String[] args) {
-     
+    int arr[]={2,3,4,5,6,7};
+    
+    int count;
+     for(int i=0;i<arr.length;i++){
+      count =i;
+      for(int j=i+1;j<arr.length;j++){
+        System.out.print("("+arr[count]+","+arr[j]+ ") ");
+      }
+      System.out.println();
+    }
+
+    //  int count;
+    //  for(int i=0;i<arr.length;i++){
+    //   count =arr[i];
+    //   for(int j=i+1;j<arr.length;j++){
+    //     System.out.print("("+count+","+arr[j]+ ") ");
+       
+    //   }
+    //  }
   }
 }
