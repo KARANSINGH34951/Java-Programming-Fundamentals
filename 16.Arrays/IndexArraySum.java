37d4a8b6a7bfd20dd1573c5cfd29f6066a3b02de
karan singh D <ceitkaransingh2026@gmail.com>
2024-08-23 21:51:06 +0530

    Array based Questions

---
+++ 16.Arrays/IndexArraySum.java
@@ -0,0 +1,23 @@
+public class IndexArraySum {
+  public static void main(String[] args) {
+    int arr[]={1,2,3,4};
+    int start,end;
+    int currentsum=0;
+    int maxsum=0;
+
+    int arrfix[]=new int[arr.length];
+    for(int i=1;i<arr.length;i++){
+      arrfix[i]=arr[i-1]-arr[i];
+    }
+
+    for(int i=0;i<arr.length;i++){
+      start=i;
+      for(int j=0;j<arr.length;j++){
+        end=j;
+
+        prefix[end]-pref
+
+      }
+    }
+  }
+}
--- 16.Arrays/arraysum.java
+++ 16.Arrays/arraysum.java
@@ -2,17 +2,27 @@ public class arraysum {
   public static void main(String[] args) {
     int numbers[]={1,2,3,4,5};
     int start,end;
-
+    int currentsum=0;
+    int maxsum=Integer.MIN_VALUE;
+   
     for(int i=0;i<numbers.length;i++){
       start=i;
       for(int j=i;j<numbers.length;j++){
-        end=j;
+        end =j;
+        currentsum=0;
         for(int k=start;k<=end;k++){
-          System.out.print(numbers[k]);
+          // System.out.print(numbers[k]+" ");
+          currentsum+=numbers[k];
+          System.out.print(currentsum);
+        }
+        if(maxsum<currentsum){
+          maxsum=currentsum;
         }
         System.out.println();
       }
       System.out.println();
     }
+
+    System.out.println(maxsum);
   }
 }
