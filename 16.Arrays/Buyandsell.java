58420381426ebdc9461579b39fde33b71021d7d9
karan singh D <ceitkaransingh2026@gmail.com>
2024-08-30 22:28:36 +0530

    Sorting Algo

---
+++ 16.Arrays/Buyandsell.java
@@ -0,0 +1,19 @@
+public class Buyandsell {
+  public static void main(String[] args) {
+    int price[]={7,1,5,3,6,4};
+
+    int buyprice=Integer.MAX_VALUE;
+    int maxprofit=0;
+
+    for(int i=0;i<price.length;i++){
+      if(buyprice<price[i]){ //profit
+        int profit=price[i]-buyprice;
+        maxprofit=Math.max(maxprofit,profit);
+      }
+      else{
+        buyprice=price[i];
+      }
+    }
+    System.out.println(maxprofit);
+  }
+}
---
+++ 17.sorting/Bubblesort.java
@@ -0,0 +1,23 @@
+
+
+public class Bubblesort {
+  public static void main(String[] args) {
+    int arr[]={2,3,4,1,56,43,22,13};
+    int tmp;
+
+    for(int i=0;i<arr.length-1;i++){
+      for(int j=0;j<arr.length-1-i;j++){
+        if(arr[j]>arr[j+1]){
+          tmp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=tmp;
+        }
+      }
+    }
+
+    for(int i=0;i<arr.length;i++){
+      System.out.println(arr[i]);
+    }
+    
+  }
+}
