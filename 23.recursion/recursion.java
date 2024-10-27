aa54e4de92672701d70e492677bc883469445967
karan singh D <ceitkaransingh2026@gmail.com>
2024-10-27 23:21:24 +0530

    More on recusion prombles

--- 23.recursion/recursion.java
+++ 23.recursion/recursion.java
@@ -72,3 +72,15 @@ class factorials{
   }
 }
 
+public static boolean checkPalindrome(int n)
+    {
+        int reverse = 0;
+        
+        int temp = n;
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+        
+        return (reverse == n);
+    }
